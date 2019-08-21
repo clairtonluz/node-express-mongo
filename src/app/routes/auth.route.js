4d38c3739f78ea579139a1eae77b0e6604e93f09
Clairton Luz <clairton.c.l@gmail.com>
2019-08-20 22:25:12 -0300

    refresh token

--- src/app/routes/auth.route.js
+++ src/app/routes/auth.route.js
@@ -19,7 +19,7 @@ const createToken = (id, name, email) => {
   const token = jwt.sign(userData, config.jwt.secret, tokenData);
   const refreshToken = jwt.sign(userData, config.jwt.refreshSecret, refreshTokenData);
   return {
-    user,
+    user: { id, name, email },
     token: { value: token, expiresIn: config.jwt.expiresIn - 1 },
     refreshToken: { value: refreshToken, expiresIn: config.jwt.refreshExpiresIn - 1 },
   };
@@ -39,38 +39,22 @@ router.post('/login', function (req, res, next) {
     }
 
     req.login(user, { session: false }, (err) => {
+      console.log('teste');
       if (err) {
         res.send(err);
       }
-      const userData = { name: user.name, email: user.email };
-      const tokenData = {
-        subject: `${user.id}`,
-        issuer: config.jwt.issuer,
-        expiresIn: `${config.jwt.expiresIn}s`,
-      };
-      const refreshTokenData = {
-        subject: `${user.id}`,
-        issuer: config.jwt.issuer,
-        expiresIn: `${config.jwt.refreshExpiresIn}s`,
-      };
+      const tokens = createToken(user.id, user.name, user.email);
 
-      const token = jwt.sign(userData, config.jwt.secret, tokenData);
-      const refreshToken = jwt.sign(userData, config.jwt.refreshSecret, tokenData);
-      return res.json({
-        user,
-        token: { value: token, expiresIn: config.jwt.expiresIn - 1 },
-        refreshToken: { value: refreshToken, expiresIn: config.jwt.refreshExpiresIn - 1 },
-      });
+      return res.json(tokens);
     });
   })(req, res);
 });
 
 
 router.post('/refresh', function (req, res, next) {
-  // console.log('req.user', req);
-  res.send({
-    user: req.user,
-    teste: "testse",
-  });
+  const user = req.user;
+  if (!user) throw new Error('Usuário não encontrado');
+  const tokens = createToken(user.sub, user.name, user.email);
+  res.send(tokens);
 });
 module.exports = router;
